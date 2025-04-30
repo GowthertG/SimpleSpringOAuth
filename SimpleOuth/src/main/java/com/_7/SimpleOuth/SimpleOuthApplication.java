da09d5e8cc54eda720b9cc6a1339c94097c1adf6
Gowthertg <houssamhaddouche777@gmail.com>
2025-04-30 12:49:01 +0100

    Initial commit with Spring Boot Web, Security, and Client OAuth setup

---
+++ SimpleOuth/src/main/java/com/_7/SimpleOuth/SimpleOuthApplication.java
@@ -0,0 +1,13 @@
+package com._7.SimpleOuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOuthApplication.class, args);
+	}
+
+}
---
+++ SimpleOuth/src/test/java/com/_7/SimpleOuth/SimpleOuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com._7.SimpleOuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
